6a350d41e5cdee6c1dff28d8e4b3e964c33ff13c
E Sequeira <5458743+geseq@users.noreply.github.com>
2022-10-11 07:46:26 +0100

    add naive latency bench

--- cmd/bench/main.go
+++ cmd/bench/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"runtime"
+	"runtime/debug"
+	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 
+	"fortio.org/fortio/stats"
+	"github.com/aristanetworks/goarista/monotime"
 	"github.com/geseq/orderbook"
 	decimal "github.com/geseq/udecimal"
 )
@@ -33,64 +41,92 @@ func getPrice(bid, ask, diff decimal.Decimal, dec bool) (decimal.Decimal, decima
 }
 
 func main() {
-	seed := flag.Int64("seed", time.Now().UnixNano(), "rand seed")
-	duration := flag.Int("duration", 0, "benchmark duration in seconds")
-	lb := flag.String("l", "50.0", "lower bound")
-	ub := flag.String("u", "100.0", "upper bound")
-	ms := flag.String("m", "0.25", "min spread")
-	pd := flag.Uint64("p", 10, "print duration in seconds")
-	flag.Parse()
+	runtime.LockOSThread()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		runtime.LockOSThread()
+		log.Printf("Running on thread %d", syscall.Gettid())
+		seed := flag.Int64("seed", time.Now().UnixNano(), "rand seed")
+		duration := flag.Int("duration", 0, "benchmark duration in seconds")
+		lb := flag.String("l", "50.0", "lower bound")
+		ub := flag.String("u", "100.0", "upper bound")
+		ms := flag.String("m", "0.25", "min spread")
+		pd := flag.Uint64("p", 10, "print duration in seconds")
+		gc := flag.Bool("gc", true, "use gc")
+		flag.Parse()
 
-	rand := rand.New(rand.NewSource(*seed))
+		if !*gc {
+			debug.SetGCPercent(-1)
+		}
+		rand := rand.New(rand.NewSource(*seed))
 
-	lowerBound := decimal.MustParse(*lb)
-	upperBound := decimal.MustParse(*ub)
-	minSpread := decimal.MustParse(*ms)
+		lowerBound := decimal.MustParse(*lb)
+		upperBound := decimal.MustParse(*ub)
+		minSpread := decimal.MustParse(*ms)
 
-	bid := lowerBound.Add(upperBound).Div(decimal.NewI(2, 0))
-	ask := bid.Sub(minSpread)
-	bidQty := decimal.NewI(10, 0)
-	askQty := decimal.NewI(10, 0)
+		bid := lowerBound.Add(upperBound).Div(decimal.NewI(2, 0))
+		ask := bid.Sub(minSpread)
+		bidQty := decimal.NewI(10, 0)
+		askQty := decimal.NewI(10, 0)
 
-	on := &EmptyNotification{}
-	ob := orderbook.NewOrderBook(on)
+		on := &EmptyNotification{}
+		ob := orderbook.NewOrderBook(on)
+		ah := stats.NewHistogram(10, 1)
+		ch := stats.NewHistogram(10, 1)
 
-	var tok, buyID, sellID uint64
-	var ops uint64
+		var tok, buyID, sellID uint64
+		var ops uint64
 
-	start := time.Now()
-	end := time.Now().Add(time.Duration(*duration) * time.Second)
-	diff := time.Duration(0)
-	for time.Now().Before(end) {
-		var r = rand.Intn(10)
-		dec := r < 5
+		start := time.Now()
+		end := time.Now().Add(time.Duration(*duration) * time.Second)
+		var diff uint64
+		for time.Now().Before(end) {
+			var r = rand.Intn(10)
+			dec := r < 5
 
-		bid, ask = getPrice(bid, ask, minSpread, dec)
-		if bid.LessThan(lowerBound) {
-			bid, ask = getPrice(bid, ask, minSpread, false)
-		} else if bid.GreaterThan(upperBound) {
-			bid, ask = getPrice(bid, ask, minSpread, true)
+			bid, ask = getPrice(bid, ask, minSpread, dec)
+			if bid.LessThan(lowerBound) {
+				bid, ask = getPrice(bid, ask, minSpread, false)
+			} else if bid.GreaterThan(upperBound) {
+				bid, ask = getPrice(bid, ask, minSpread, true)
+			}
+
+			ds := monotime.Now()
+			tok = tok + 1
+			s := monotime.Now()
+			ob.CancelOrder(tok, buyID)
+			ch.Record(float64(monotime.Now() - s))
+			tok = tok + 1
+			s = monotime.Now()
+			ob.CancelOrder(tok, sellID)
+			ch.Record(float64(monotime.Now() - s))
+			tok = tok + 1
+			buyID = tok
+			tok = tok + 1
+			sellID = tok
+			s = monotime.Now()
+			ob.ProcessOrder(buyID, buyID, orderbook.Limit, orderbook.Buy, bidQty, bid, decimal.Zero, orderbook.None)
+			ah.Record(float64(monotime.Now() - s))
+			s = monotime.Now()
+			ob.ProcessOrder(sellID, sellID, orderbook.Limit, orderbook.Sell, askQty, ask, decimal.Zero, orderbook.None)
+			ah.Record(float64(monotime.Now() - s))
+			diff += monotime.Now() - ds
+			atomic.AddUint64(&ops, 4) // 4 cancels and adds
+
+			if uint64(time.Now().Sub(start).Seconds()) > *pd {
+				fmt.Printf("ops/s: %d\n", ops/uint64(diff/1000_000_000))
+				ops = 0
+				start = time.Now()
+				diff = 0
+			}
 		}
 
-		ds := time.Now()
-		tok = tok + 1
-		ob.CancelOrder(tok, buyID)
-		tok = tok + 1
-		ob.CancelOrder(tok, sellID)
-		tok = tok + 1
-		buyID = tok
-		tok = tok + 1
-		sellID = tok
-		ob.ProcessOrder(buyID, buyID, orderbook.Limit, orderbook.Buy, bidQty, bid, decimal.Zero, orderbook.None)
-		ob.ProcessOrder(sellID, sellID, orderbook.Limit, orderbook.Sell, askQty, ask, decimal.Zero, orderbook.None)
-		diff += time.Now().Sub(ds)
-		atomic.AddUint64(&ops, 4) // 4 cancels and adds
+		ah.Print(os.Stdout, "Add Results", []float64{50, 75, 90, 95, 99, 99.9, 99.99, 100})
+		ch.Print(os.Stdout, "Cancel Results", []float64{50, 75, 90, 95, 99, 99.9, 99.99, 100})
 
-		if uint64(time.Now().Sub(start).Seconds()) > *pd {
-			fmt.Printf("ops/s: %d\n", ops/uint64(diff.Seconds()))
-			ops = 0
-			start = time.Now()
-			diff = 0
-		}
-	}
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
