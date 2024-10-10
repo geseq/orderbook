37a349604c696ab32dddf5e69a6d1a0b2b21bf6c
Andrey Maltsev <maltsev.a2012@gmail.com>
2024-10-10 11:45:24 +0200

    implement generic pool & generic tree

--- pricelevel.go
+++ pricelevel.go
@@ -1,6 +1,8 @@
 package orderbook
 
 import (
+	local_tree "github.com/geseq/orderbook/pkg/tree"
+	"github.com/geseq/udecimal"
 	decimal "github.com/geseq/udecimal"
 )
 
@@ -8,7 +10,7 @@ import (
 
 // priceLevel implements facade to operations with order queue
 type priceLevel struct {
-	priceTree *tree
+	priceTree *local_tree.Tree[decimal.Decimal, *orderQueue]
 	priceType PriceType
 
 	volume    decimal.Decimal
@@ -41,7 +43,7 @@ const (
 // newPriceLevel creates new priceLevel manager
 func newPriceLevel(priceType PriceType) *priceLevel {
 	return &priceLevel{
-		priceTree: newWithTree(Comparator),
+		priceTree: local_tree.NewWithTree[udecimal.Decimal, *orderQueue](Comparator, 1),
 		priceType: priceType,
 		volume:    decimal.Zero,
 	}
